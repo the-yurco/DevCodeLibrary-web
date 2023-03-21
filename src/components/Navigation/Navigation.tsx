f18738cdca704680a65b408221b8b7c2a4622846
georgee <jurajbosy1@gmail.com>
2023-03-22 00:24:33 +0100

    I have made rought construction for NAVIGATION on each site

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,20 +1,44 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import SingIn from "../../pages/sing-in/SingIn";
 
 const Navigation = () => {
   return (
     <>
-      <header className="header-top-strip py-3 bg-primary">
-        <div className="container-xxl">
-          <div className="row">
-            <div className="col-6">
-              <p className="text-white mb-0 mx-3 ">NAVIGATION LINKS</p>
-            </div>
-            <div className="col-6">
-              <p className="text-end text-white mb-0">NAVIGATION LINKS</p>
-            </div>
-          </div>
-        </div>
-      </header>
+      <Navbar
+        bg="transparent"
+        variant="dark"
+        className="d-flex justify-content-between "
+      >
+        <Navbar.Brand href="/" className="py-1 px-3 brand_link rounded-1">
+          DevCodeLibrary
+        </Navbar.Brand>
+        <Nav className="d-flex w-50 navbar_links">
+          <Nav.Link href="/about" className="link">
+            About
+          </Nav.Link>
+          <Nav.Link href="/library" className="link">
+            Library
+          </Nav.Link>
+          <Nav.Link href="/support" className="link">
+            Support us
+          </Nav.Link>
+          <Nav.Link href="/contact" className="link">
+            Contact
+          </Nav.Link>
+        </Nav>
+        <Nav className="d-flex gap-3">
+          <Navbar.Brand
+            href="/signin"
+            id="signin"
+            className="py-1 px-4 rounded-1"
+          >
+            Sign In
+          </Navbar.Brand>
+        </Nav>
+      </Navbar>
     </>
   );
 };
