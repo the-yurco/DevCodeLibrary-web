835c2bf9a8b7782777b2e7ade53526082b5267c3
georgee <jurajbosy1@gmail.com>
2023-03-22 22:19:28 +0100

    Hero section - rought build almost DONE

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BsFillPlayFill } from "react-icons/bs";
 // import Hero from "../../components/Hero/Hero";
 
 const Home = () => {
@@ -6,24 +7,39 @@ const Home = () => {
     <div>
       <>
         <section className="home-hero py-5">
-          <div className="container-xxl">
+          <div className="container-fluid">
             <div className="row">
-              <div className="col-6">
+              <div className="col-6 pe-4">
                 <div className="main-banner mt-5">
-                  <h1>Learn Fast and Effectivelly</h1>
-                  <br />
-                  <h1>
-                    We are library of everything you need for coding{" "}
-                    <span className="px-2">FREE</span>
-                  </h1>
-                  <br />
-                  <br />
-                  <h4>Discover Everything you need to know...</h4>
+                  <div>
+                    <h1>Learn Fast and Effectivelly</h1>
+                    <br />
+                    <h1>
+                      We are library of everything you need for coding{" "}
+                      <span className="px-2">FREE</span>
+                    </h1>
+                    <br />
+                    <br />
+                    <h4>Discover Everything you need to know...</h4>
+                  </div>
+                  <a
+                    href="#"
+                    className="btn btn-primary py-2  mt-3 d-flex align-items-center justify-content-center w-25 gap-2"
+                  >
+                    <BsFillPlayFill /> Watch
+                  </a>
                 </div>
               </div>
               <div className="col-6">
-                <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
-                  <div className="small-banner"></div>
+                <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center ">
+                  <div className="small-banner">
+                    <img
+                      src="src/assets/images/2211.w026.n002.2759B.p1.2759.jpg"
+                      alt=""
+                      height={480}
+                      className="rounded"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
