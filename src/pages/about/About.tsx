0df15aa0625122761edccaee17e40d2e7e0103a7
georgee <jurajbosy1@gmail.com>
2023-03-25 19:07:15 +0100

    added text for about page | need to style lit a bit

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <h2 className="mt-5"> March 21 [ DevCodeLibrary ]</h2>
+      <br />
+      <h6 className="text-center">
+        I had this project in my mind for a long time, but only this day I
+        started programming this project. The reason why I didn't start sooner
+        was because of schoolwork, project preparation, other unfinished
+        projects for school / for me, learning some technologies to build this
+        project etc.... Now we can go to the content and description of this
+        project. DevCodeLibrary is designed for people who want to start
+        learning programming. From kids to grandparents we have all categories
+        for them, which contain groups of different levels. I created
+        DevCodeLibrary because when I started programming I didn't know where to
+        start, what direction to focus and stay, simple questions, tests or
+        challenges to test my programming knowledge. Yes, this is a site for
+        those but also for those who want to improve because as I said it is
+        designed for every level. And it works by storing videos of possible
+        tutorials that would be most effective for learning programming. Of
+        course we will also have our Tips, Tricks, Tutorials etc. but for now it
+        will be like a big repository of Tutorials that we think are the most
+        effective for learning. We don't want to offend anyone in advance if
+        their video is not on our site, but also if you have a problem with your
+        video being on our site you should contact us. The project is still in
+        the process of rebuilding so don't be surprised if there will be less
+        problems with the connection to the site. Translated with
+        www.DeepL.com/Translator (free version)
+      </h6>
+    </>
+  );
 };
 
 export default About;
