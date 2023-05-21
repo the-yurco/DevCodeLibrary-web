e31e7da57699d1e8ee9cbedf0b9c044c452221c6
jurco-exe <jurajbosy1@gmail.com>
2023-05-22 01:19:48 +0200

    feat: update for library container

--- src/pages/library/library/LibraryMain.tsx
+++ src/pages/library/library/LibraryMain.tsx
@@ -2,9 +2,57 @@ import React from 'react';
 import Card_layout from '../../../components/Card/Card_main';
 import { Row, Col } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import Card_main from '../../../components/Card/Card_main';
 
 const LibraryMain = () => {
-	return <></>;
+	return (
+		<>
+			<Container className="library-container">
+				<Row className="row-container">
+					<Col lg={3} sm={6} md={4}>
+						<Card_main
+							imgUrl={''}
+							name={'jebem na to '}
+							description={
+								'l;aksjdflkajsdflkjasl;dfjasl;fjalskfjkasdjflkoiewquriwuroiuqeroiuqweropiuqwroiuwqoiruwqopriuwqoiruwqoiruqwoiruwqoriuwqeoriuweoru'
+							}
+							link={''}
+						/>
+					</Col>
+					<Col lg={3} sm={6} md={4}>
+						<Card_main
+							imgUrl={''}
+							name={'jebem na to '}
+							description={
+								'l;aksjdflkajsdflkjasl;dfjasl;fjalskfjkasdjflkoiewquriwuroiuqeroiuqweropiuqwroiuwqoiruwqopriuwqoiruwqoiruqwoiruwqoriuwqeoriuweoru'
+							}
+							link={''}
+						/>
+					</Col>
+					<Col lg={3} sm={6} md={4}>
+						<Card_main
+							imgUrl={''}
+							name={'jebem na to '}
+							description={
+								'l;aksjdflkajsdflkjasl;dfjasl;fjalskfjkasdjflkoiewquriwuroiuqeroiuqweropiuqwroiuwqoiruwqopriuwqoiruwqoiruqwoiruwqoriuwqeoriuweoru'
+							}
+							link={''}
+						/>
+					</Col>
+					<Col lg={3} sm={6} md={4}>
+						<Card_main
+							imgUrl={''}
+							name={'jebem na to '}
+							description={
+								'l;aksjdflkajsdflkjasl;dfjasl;fjalskfjkasdjflkoiewquriwuroiuqeroiuqweropiuqwroiuwqoiruwqopriuwqoiruwqoiruqwoiruwqoriuwqeoriuweoru'
+							}
+							link={''}
+						/>
+					</Col>
+				</Row>
+			</Container>
+		</>
+	);
 };
 
 export default LibraryMain;
